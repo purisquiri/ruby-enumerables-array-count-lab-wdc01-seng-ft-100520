12e10b59ea8fa09958776962eda63a92497ca1dd
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-02 14:38:34 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,8 @@
 def count_strings(array)
-  array.count do |strings|
-    strings.is_a? String
+  array.count do |element|
+    element.is_a? String
+  end
+  
 end
 
 def count_empty_strings(array)
